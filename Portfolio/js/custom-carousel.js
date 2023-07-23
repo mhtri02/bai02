91c17ee782b2270ead6f3743d6ed80c4fd59ce8f
mhtri02 <140300498+mhtri02@users.noreply.github.com>
2023-07-23 13:29:18 +0700

    Add files via upload

---
+++ Portfolio/js/custom-carousel.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+ })
+$('#gallery').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
